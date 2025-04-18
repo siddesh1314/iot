38f697b0f6ef7554b162c3c2355ece9ffadac8a6
siddesh1314 <166679843+siddesh1314@users.noreply.github.com>
2025-04-18 11:05:26 +0530

    Update emergency_module.py

--- emergency_module.py
+++ emergency_module.py
@@ -3,3 +3,9 @@
 def handle_emergency():
     print("🚨 Emergency action triggered!")
     # You can add logic here like sending SMS, activating camera, etc.
+
+
+import speech_recognition as sr
+
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print(f"Device {index}: {name}")
